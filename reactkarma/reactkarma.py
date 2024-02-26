b0c926aaab7a81a3fd3babf1c675aa6b3b5d5b1c
Ahmed Siam <aahs.coder@gmail.com>
2024-02-26 22:58:17 +0200

    Change name of donor to voter

--- reactkarma/reactkarma.py
+++ reactkarma/reactkarma.py
@@ -194,19 +194,19 @@ class ReactKarma(getattr(commands, "Cog", object)):
             await self._add_karma(user, author, 1 if upvote == added else -1, reaction)
 
     async def _add_karma(
-            self, donor: discord.User, recipient: discord.User,
+            self, voter: discord.User, recipient: discord.User,
             amount: int, reaction: discord.Reaction,
     ):
         settings = self.conf.user(recipient)
         karma = await settings.karma()
         await settings.karma.set(karma + amount)
 
-        donor_name = "{0}({1})".format(donor.display_name, donor.name)
+        voter_name = "{0}({1})".format(voter.display_name, voter.name)
         recipient_name = "{0}({1})".format(recipient.display_name, recipient.name)
         action = "gave a karma to" if amount == 1 else "took a karma from"
 
         await reaction.message.channel.send(
-            "{0} {1} {2}.".format(donor_name, action, recipient_name)
+            "{0} {1} {2}.".format(voter_name, action, recipient_name)
         )
 
     async def _get_emoji_id(self, guild: discord.Guild, *, upvote: bool):
